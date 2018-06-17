69f0dafbdd6faa26bda0bc75bc592c467bfb3209
mooracle <yanuar.prakosa@gmail.com>
2018-06-17 11:42:12 +0700

    Entry 4: Lambda

--- README.md
+++ README.md
@@ -12,13 +12,15 @@ how to take advantage of the power that comes with thinking declaratively. What 
 NOTE: YOU CAN ALWAYS USE CTRL - Q to find out more on highlighted code.
 
 ## Teachers Notes
-1. Entry 3: A More Functional Loop: 
+1. Entry 3: A More Functional Loop: [course](https://teamtreehouse.com/library/a-more-functional-loop)
     1. [Iterable.forEach Javadoc](https://docs.oracle.com/javase/8/docs/api/java/lang/Iterable.html#forEach-java.util.function.Consumer-)
     2. [forEach method for iterable](https://docs.oracle.com/javase/8/docs/api/java/lang/Iterable.html#forEach-java.util.function.Consumer-)
     with the [definition of Consumer](https://docs.oracle.com/javase/8/docs/api/java/util/function/Consumer.html).
     A functional interface is an interface with exactly 1 abstract method.
+2. Entry 4: Lambda [course](https://teamtreehouse.com/library/lambdas)
     
 ## Parking Lot of this Course
 Here is some terms that hasn't fully explained:
 1. Pure
 2. Side Effects
+3. Higher Order Function
--- src/com/mooracle/Main.java
+++ src/com/mooracle/Main.java
@@ -35,6 +35,40 @@ import java.util.function.Consumer;
  * what in the middle of the @Override code ask it to do.
  *
  * Yes the code is still long and still has the long similar to manual for loop and print each loop String.
+ *
+ * Entry 4: Lambda
+ * Creating an anonymous function is the heart that of function programming. But the anonymous function implementation
+ * is not so neat. It still long and hard to remember if there was no auto input in the IDEA. Even the suggestion pop up
+ * in the IDEA suggested us to use lambda instead.
+ *
+ * Before that we need to look back at entry 3 declarative code. It was a forEach method that fed another function as
+ * parameter. This function is called consumer of a String to handle. In this case Consumer is a functional interface
+ * which means it only has one abstract method that we override in the Entry 3. The method is called accept(Sting s).
+ *
+ * Side note: a method like forEach which accepts a function as parameter is also called higher order function.
+ *
+ * Rule on writing a Lambda:
+ * - This is a long kind of lambda.
+ * - specify what you're attempting to pass into the function with parenthesis in this case String and let's call it
+ *      ingredient
+ * - Then we add an arrow with dash and greater than sign.
+ * - Followed by the body of the function wrapped inside a {} brace in this case the body is println.
+ * This commonly known as Synthetic sugar
+ *
+ * -> Then since we know that ingredient is a String since it was passed into println we can omit the String in the
+ * parameter declaration!
+ *
+ * -> Then the body is only one line code. We can just omit the {} brace and make it a one liner
+ *
+ * -> Then the ingredient is the only parameter passed. Thus we can omit the () parantesis and make it true one liner.
+ *
+ * As proof that this is an Anonymous function we can just create another Consumer functions and pass ingredient!
+ * As Consumer class object has beed defined we can just use that newly created Consumer class to process the
+ * ingredients list forEach method.
+ *
+ * Summary: Basically we just defining Consumer object (which is the only function expected by List.forEach method to
+ * pass. Thus the lambda we write as only a one liner object will be treated as Consumer. Java will trust us to take
+ * care the checking of the object passed into that lambda.
  * */
 
 public class Main {
@@ -62,11 +96,31 @@ public class Main {
 
         // entry 3; using forEach method (declarative):
 
-        ingredients.forEach(new Consumer<String>() {
+        /*ingredients.forEach(new Consumer<String>() {
             @Override
             public void accept(String s) {
                 System.out.println(s);// <- accept each String from ingredients list and print it
             }
-        });//<- link on the README or CTRL - Q after highlighting the code forEach
+        });//<- link on the README or CTRL - Q after highlighting the code forEach*/
+
+        //entry 4: long type lambda
+
+        /*ingredients.forEach((String ingredient) -> {
+            System.out.println(ingredient);
+        });*/
+
+        // entry 4: lambda omit the String parameter declaration since it knows while putting into println
+
+       /* ingredients.forEach((ingredient) -> {
+            System.out.println(ingredient);
+        });*/
+
+       // entry 4: just omit {} and () in the ingredient left hand side to create one liner:
+
+        /* ingredients.forEach(ingredient -> System.out.println(ingredient));*/
+
+        // entry 4: proof of Anonymous function
+        Consumer<String> printer = resep -> System.out.println(resep);
+        ingredients.forEach(printer);
     }
 }
